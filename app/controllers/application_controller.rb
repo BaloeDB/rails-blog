566a6fe4e9c193b6558fc9ab3caf4b00ec6c29e3
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-06-16 13:19:20 +0200

    CSRF Protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,17 @@ class ApplicationController < ActionController::Base
         return @current_user if defined?(@current_user)
         @current_user = current_user_session && current_user_session.user
       end
-  end
+
+    protected
+    
+    def handle_unverified_request
+        # raise an exception
+        fail ActionController::InvalidAuthenticityToken
+        # or destroy session, redirect
+        if current_user_session
+        current_user_session.destroy
+        end
+        redirect_to root_url
+    end
+
+end
